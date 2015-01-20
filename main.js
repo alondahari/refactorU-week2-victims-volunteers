b7888a10671bcb17696ad8362a914eb2d72d16db
Alon Dahari <climbinghobo@gmail.com>
2015-01-19 18:04:09 -0700

    basic display functionality

--- main.js
+++ main.js
@@ -1,12 +1,71 @@
 var numberOfVictims = prompt('How many victims?');
 var victimInfo = [];
 var volunteerInfo =[];
-
+var victims = document.getElementById('victim-info');
+var volunteers = document.getElementById('volunteer-info');
+var name, number, street, ol, liName, liNumber, liStreet;
 
 for (var i = 1; i <= numberOfVictims; i++) {
+  name = prompt('Victim ' + i + ' - name:');
+  number = prompt('Victim ' + i + ' - number:');
+  street = prompt('Victim ' + i + ' - street:');
+
   victimInfo.push({
-    'name': prompt('Victim ' + i + ' - name:'),
-    'number': prompt('Victim ' + i + ' - phone number:'),
-    'street': prompt('Victim ' + i + ' - street:')
+    'name': name,
+    'number': number,
+    'street': street
   });
+
+  ol = document.createElement('ol');
+
+  liName = document.createElement('li');
+  liName.textContent =  "name: " + name;
+
+  liNumber = document.createElement('li');
+  liNumber.textContent =  "number: " + number;
+
+  liStreet = document.createElement('li');
+  liStreet.textContent =  "street: " + street;
+
+  victims.appendChild(ol);
+
+  ol.appendChild( liName );
+  ol.appendChild( liNumber );
+  ol.appendChild( liStreet );
 }
+var numberOfVolunteers = prompt('How many volunteers?');
+
+for (var i = 1; i <= numberOfVolunteers; i++) {
+  name = prompt('Volunteer ' + i + ' - name:');
+  number = prompt('Volunteer ' + i + ' - number:');
+  street = prompt('Volunteer ' + i + ' - street:');
+
+  volunteerInfo.push({
+    'name': name,
+    'number': number,
+    'street': street
+  });
+
+  ol = document.createElement('ol');
+
+  liName = document.createElement('li');
+  liName.textContent =  "name: " + name;
+
+  liNumber = document.createElement('li');
+  liNumber.textContent =  "number: " + number;
+
+  liStreet = document.createElement('li');
+  liStreet.textContent =  "street: " + street;
+
+  volunteers.appendChild(ol);
+
+  ol.appendChild( liName );
+  ol.appendChild( liNumber );
+  ol.appendChild( liStreet );
+}
+
+document.getElementById('number-of-victims').textContent =
+'Number of victims: ' + numberOfVictims;
+
+document.getElementById('number-of-volunteers').textContent =
+'Number of volunteers: ' + numberOfVictims;
