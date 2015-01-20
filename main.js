2909a3a0b10fc7df385eb2a69ba87c10bfdfd16e
Alon Dahari <climbinghobo@gmail.com>
2015-01-19 18:18:18 -0700

    refactor and restructure

--- main.js
+++ main.js
@@ -3,12 +3,37 @@ var victimInfo = [];
 var volunteerInfo =[];
 var victims = document.getElementById('victim-info');
 var volunteers = document.getElementById('volunteer-info');
-var name, number, street, ol, liName, liNumber, liStreet;
+var name, number, street, ul, liName, liNumber, liStreet;
+
+function getData(value, i) {
+  name = prompt(value + ' ' + i + ' - name:');
+  number = prompt(value + ' ' + i + ' - number:');
+  street = prompt(value + ' ' + i + ' - street:');
+}
+
+function displayData(list, value, i) {
+  ul = document.createElement('ul');
+  list.appendChild(ul);
+  ul.textContent = value + ' ' + i + ':';
+
+  liName = document.createElement('li');
+  liName.textContent =  "name: " + name;
+
+  liNumber = document.createElement('li');
+  liNumber.textContent =  "number: " + number;
+
+  liStreet = document.createElement('li');
+  liStreet.textContent =  "street: " + street;
+
+  ul.appendChild( liName );
+  ul.appendChild( liNumber );
+  ul.appendChild( liStreet );
+
+}
 
 for (var i = 1; i <= numberOfVictims; i++) {
-  name = prompt('Victim ' + i + ' - name:');
-  number = prompt('Victim ' + i + ' - number:');
-  street = prompt('Victim ' + i + ' - street:');
+
+  getData('victim', i);
 
   victimInfo.push({
     'name': name,
@@ -16,29 +41,13 @@ for (var i = 1; i <= numberOfVictims; i++) {
     'street': street
   });
 
-  ol = document.createElement('ol');
-
-  liName = document.createElement('li');
-  liName.textContent =  "name: " + name;
-
-  liNumber = document.createElement('li');
-  liNumber.textContent =  "number: " + number;
-
-  liStreet = document.createElement('li');
-  liStreet.textContent =  "street: " + street;
-
-  victims.appendChild(ol);
-
-  ol.appendChild( liName );
-  ol.appendChild( liNumber );
-  ol.appendChild( liStreet );
+  displayData(victims, i);
 }
+
 var numberOfVolunteers = prompt('How many volunteers?');
 
 for (var i = 1; i <= numberOfVolunteers; i++) {
-  name = prompt('Volunteer ' + i + ' - name:');
-  number = prompt('Volunteer ' + i + ' - number:');
-  street = prompt('Volunteer ' + i + ' - street:');
+  getData('volunteer', i);
 
   volunteerInfo.push({
     'name': name,
@@ -46,26 +55,11 @@ for (var i = 1; i <= numberOfVolunteers; i++) {
     'street': street
   });
 
-  ol = document.createElement('ol');
-
-  liName = document.createElement('li');
-  liName.textContent =  "name: " + name;
-
-  liNumber = document.createElement('li');
-  liNumber.textContent =  "number: " + number;
-
-  liStreet = document.createElement('li');
-  liStreet.textContent =  "street: " + street;
-
-  volunteers.appendChild(ol);
-
-  ol.appendChild( liName );
-  ol.appendChild( liNumber );
-  ol.appendChild( liStreet );
+  displayData(volunteers, 'volunteer', i);
 }
 
 document.getElementById('number-of-victims').textContent =
 'Number of victims: ' + numberOfVictims;
 
 document.getElementById('number-of-volunteers').textContent =
-'Number of volunteers: ' + numberOfVictims;
+'Number of volunteers: ' + numberOfVolunteers;
